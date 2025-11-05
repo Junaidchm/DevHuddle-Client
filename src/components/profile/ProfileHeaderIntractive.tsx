6bd2969881b657b2210eb68a51815b8d790bb207
junaid <mac@junaids-MacBook-Air.local>
2025-11-05 20:15:54 +0530

    fix: bug fix for query Client

--- src/components/profile/ProfileHeaderIntractive.tsx
+++ src/components/profile/ProfileHeaderIntractive.tsx
@@ -1,23 +1,23 @@
-// app/components/ProfileHeaderInteractive.tsx
-'use client';
-import React from 'react';
-import Avatar from './Avatar';
-import ActionButtons from './ActionButtons';
+// // app/components/ProfileHeaderInteractive.tsx
+// 'use client';
+// import React from 'react';
+// import Avatar from './Avatar';
+// import ActionButtons from './ActionButtons';
 
-interface ProfileHeaderInteractiveProps {
-  imgSrc: string;
-  alt: string;
-  userId?: string;
-  isOwnProfile: boolean;
-}
+// interface ProfileHeaderInteractiveProps {
+//   imgSrc: string;
+//   alt: string;
+//   userId?: string;
+//   isOwnProfile: boolean;
+// }
 
-const ProfileHeaderInteractive = ({ imgSrc, alt, userId, isOwnProfile }: ProfileHeaderInteractiveProps) => {
-  return (
-    <>
-      <Avatar imgSrc={imgSrc} alt={alt} />
-      {userId && <ActionButtons userId={userId} isOwnProfile={isOwnProfile} />}
-    </>
-  );
-};
+// const ProfileHeaderInteractive = ({ imgSrc, alt, userId, isOwnProfile }: ProfileHeaderInteractiveProps) => {
+//   return (
+//     <>
+//       <Avatar imgSrc={imgSrc} alt={alt} />
+//       {userId && <ActionButtons userId={userId} isOwnProfile={isOwnProfile} />}
+//     </>
+//   );
+// };
 
-export default ProfileHeaderInteractive;
+// export default ProfileHeaderInteractive;
